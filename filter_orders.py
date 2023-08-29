fae1b712fa31dd7c4bfb216c33ef5efb3e4760c9
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-08-29 00:29:03 -0300

    added filter_orders.py error handling

--- filter_orders.py
+++ filter_orders.py
@@ -11,16 +11,20 @@ def filter_and_group_by_family(orders_by_shop):
     :param orders_by_shop: Dictionary with shop names as keys and list of orders as values.
     :return: Dictionary with shop names as keys and orders grouped by families as values.
     """
-    grouped_orders = {}
-    for shop, orders in orders_by_shop.items():
-        # Dictionary to hold orders grouped by family
-        family_group = {}
-        for order in orders:
-            item_name = clean_text(order['item'])
-            family = FAMILY_MAPPING.get(item_name, item_name)  # Use the item name as default if no family is found
-            if family not in family_group:
-                family_group[family] = []
-            family_group[family].append(order)
-        
-        grouped_orders[shop] = family_group
-    return grouped_orders
+    try:
+        grouped_orders = {}
+        for shop, orders in orders_by_shop.items():
+            # Dictionary to hold orders grouped by family
+            family_group = {}
+            for order in orders:
+                item_name = clean_text(order['item'])
+                family = FAMILY_MAPPING.get(item_name, item_name)  # Use the item name as default if no family is found
+                if family not in family_group:
+                    family_group[family] = []
+                family_group[family].append(order)
+            
+            grouped_orders[shop] = family_group
+        return grouped_orders
+
+    except Exception as e:
+        raise Exception(f"Error in filter_and_group_by_family function: {e}")
