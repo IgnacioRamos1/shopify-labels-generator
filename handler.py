7591115a851e672a70a52e80c9e7346febe1aa6b
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-08-29 00:17:34 -0300

    added lambda.py error handling

--- handler.py
+++ handler.py
@@ -4,18 +4,23 @@ import asyncio
 
 
 def lambda_handler(event, context):
-    credentials = [
-        {
-            'shop_name': 'Strawberry Store',
-            'shop_url': 'www.strawberrystorearg.com',
-            'access_token': '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'
-        },
-        # Add more stores as needed
-    ]
+    try:
+        credentials = [
+            {
+                'shop_name': 'Strawberry Store',
+                'shop_url': 'www.strawberrystorearg.com',
+                'access_token': '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'
+            },
+            # Add more stores as needed
+        ]
 
-    asyncio.get_event_loop().run_until_complete(process_orders(credentials))
+        asyncio.get_event_loop().run_until_complete(process_orders(credentials))
 
-    return {
-        'statusCode': 200,
-        'body': "CSV files generated and saved to S3"
-    }
+        return {
+            'statusCode': 200,
+            'body': "CSV files generated and saved to S3"
+        }
+
+    except Exception as e:
+        # This is a placeholder. We will add error notification here.
+        raise e
