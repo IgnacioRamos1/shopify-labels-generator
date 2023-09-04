fa7e7dab3bb5951c0f21cbaa80ed98e8b8656f31
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-09-04 11:14:53 -0300

    save zip instead of multiple csv's on S3 bucket

--- save_to_bucket.py
+++ save_to_bucket.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import asyncio
 
 
-def save_to_s3(bucket_name, content, item_name):
+def save_to_s3(bucket_name, content, file_name):
     try:
         # Convert the bucket name to a valid S3 bucket name
         bucket_name = bucket_name.lower().replace(" ", "-")
@@ -20,31 +20,23 @@ def save_to_s3(bucket_name, content, item_name):
 
         # Check if the bucket exists
         if not bucket_exists(bucket_name):
-            try:
-                s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'sa-east-1'})
-            except Exception as e:
-                raise Exception(f"Error creating bucket: {e}")
+            s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'sa-east-1'})
 
-        # Define the filename based on date and product name
-        date_str = datetime.now().strftime('%Y-%m-%d')
-        file_name = f"{bucket_name} - {date_str} - {item_name}.csv"
-        s3_key = f"{date_str}/{file_name}"  # Defining the folder/filename structure
+        # Determine the content type based on the file extension
+        content_type = 'application/zip' if file_name.endswith('.zip') else 'text/csv'
 
         # Upload the content to the bucket
-        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=content)
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=content, ContentType=content_type)
 
-        return f"File saved to {bucket_name}/{s3_key}"
+        return f"File saved to {bucket_name}/{file_name}"
 
     except Exception as e:
         raise Exception(f"Error in save_to_s3 function: {e}")
 
 
-async def async_save_to_s3(shop, product, grouped_data, credentials):
+async def async_save_to_s3(shop, product, grouped_data):
     try:
-        # Filter orders that have already been processed
         unprocessed_orders = [order for order in grouped_data[shop][product] if not check_order_processed(shop, order["order_id"], order["item_id"])]
-
-        # If all orders for this group have been processed before, return
         if not unprocessed_orders:
             return None, None, []
 
@@ -55,15 +47,11 @@ async def async_save_to_s3(shop, product, grouped_data, credentials):
         date_str = datetime.now().strftime('%Y-%m-%d')
         file_name = f"{shop} - {date_str} - {product}.csv"
         
-        # Save to S3
-        save_to_s3(shop, csv_output, product)
-
         # Mark each order as processed only if it's not marked as 'exclude'
         for order in unprocessed_orders:
             if not order.get('exclude', False):
                 mark_order_as_processed(shop, order["order_id"], order["item_id"])
 
-        # Return csv data, filename, and not added products
         return csv_output, file_name, not_added
 
     except Exception as e:
@@ -72,21 +60,21 @@ async def async_save_to_s3(shop, product, grouped_data, credentials):
 
 async def process_orders(credentials):
     try:
-        date = datetime.now().strftime('%d-%m-%Y')
+        date = datetime.now().strftime('%Y-%m-%d')
         total_orders, total_orders_count = await request_orders(credentials)
         grouped_orders = filter_and_group_by_family(total_orders)
         from_email = get_parameter('from_email')
 
-        all_not_added = []  # List to collect all not added products
+        all_not_added = []
 
         for shop in grouped_orders:
             tasks = []
             in_memory_csvs = {}
             orders_breakdown = {}
             
-            # Initiate tasks to save to S3
             for product, orders in grouped_orders[shop].items():
-                task = asyncio.ensure_future(async_save_to_s3(shop, product, grouped_orders, credentials))
+                # Removed 'credentials' from the function call
+                task = asyncio.ensure_future(async_save_to_s3(shop, product, grouped_orders))
                 tasks.append(task)
 
             completed_tasks = await asyncio.gather(*tasks)
@@ -96,20 +84,18 @@ async def process_orders(credentials):
                 if csv_data and file_name and len(csv_data.splitlines()) > 1:
                     in_memory_csvs[file_name] = csv_data
 
-            # Update the order breakdown based on the CSVs generated
             for file_name, csv_content in in_memory_csvs.items():
-                product_name = file_name.split('-')[2].strip()  # Extract product name from the filename
-                orders_breakdown[product_name] = len(csv_content.splitlines()) - 1  # Subtract 1 for the header
+                product_name = file_name.split('-')[2].strip()
+                orders_breakdown[product_name] = len(csv_content.splitlines()) - 1
 
-            # Update total orders count for the shop based on the updated orders_breakdown
             total_orders_count[shop] = sum(orders_breakdown.values())
 
             if in_memory_csvs:
-                zip_name, zip_buffer = create_zip_in_memory(shop, in_memory_csvs)
+                _, zip_buffer = create_zip_in_memory(shop, in_memory_csvs)
+                save_to_s3(shop, zip_buffer.getvalue(), f"{date}.zip")
                 to_email = [store["email"] for store in credentials if store["shop_name"] == shop][0]
-                send_email(zip_buffer, zip_name, from_email, to_email, shop, total_orders_count[shop], date, orders_breakdown)
+                send_email(zip_buffer, f"{date}.zip", from_email, to_email, shop, total_orders_count[shop], date, orders_breakdown)
 
-        # If there are products not added, send the missing products email for all of them
         if all_not_added:
             to_email = [store["email"] for store in credentials if store["shop_name"] == shop][0]
             send_products_missing_email(from_email, to_email, all_not_added)
