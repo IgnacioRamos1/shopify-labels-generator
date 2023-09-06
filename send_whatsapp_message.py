c9d2adc3efc2a92e10fb9e8ebe03178848025e7e
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-09-06 16:54:12 -0300

    comented out groups id

--- send_whatsapp_message.py
+++ send_whatsapp_message.py
@@ -9,7 +9,7 @@ def send_whatsapp_group_message(date, shop, total_orders_count, s3_presigned_url
     try:
         url = "https://api.ultramsg.com/instance60273/messages/document"
 
-        group_chat_id = "120363150899530481@g.us"
+        # group_chat_id = "120363150899530481@g.us"
 
         # The group_chat_id is now coming from the function parameter, so no need to get it again
 
@@ -32,7 +32,7 @@ def send_whatsapp_message(group_chat_id, body):
     try:
         url = "https://api.ultramsg.com/instance60273/messages/chat"
 
-        group_chat_id = "120363150899530481@g.us"
+        # group_chat_id = "120363150899530481@g.us"
 
         payload = f"token={token}&to={group_chat_id}&body={body}"
         payload = payload.encode('utf8').decode('iso-8859-1')
