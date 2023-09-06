fb37ce0539b2bd4f6b570eeee4ce2f1dec378550
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-09-06 17:33:44 -0300

    added 30 days lifecycle to s3 buckets

--- save_to_bucket.py
+++ save_to_bucket.py
@@ -3,6 +3,28 @@ from utils import bucket_exists
 import boto3
 
 
+def set_bucket_lifecycle(bucket_name):
+    try:
+        s3 = boto3.client('s3', region_name='sa-east-1')
+
+        lifecycle_policy = {
+            "Rules": [
+                {
+                    "ID": "ExpireOldFiles",
+                    "Status": "Enabled",
+                    "Expiration": {"Days": 30},
+                    "Prefix": ''  # Esto aplica la regla a todos los objetos en el bucket
+                }
+            ]
+        }
+
+        s3.put_bucket_lifecycle_configuration(Bucket=bucket_name, LifecycleConfiguration=lifecycle_policy)
+        print('Bucket lifecycle policy set successfully')
+    except Exception as e:
+        print(f"Error in set_bucket_lifecycle function: {e}")
+        raise Exception(f"Error in set_bucket_lifecycle function: {e}")
+
+
 def save_to_s3(bucket_name, content, file_name):
     try:
         # Convert the bucket name to a valid S3 bucket name
@@ -14,6 +36,7 @@ def save_to_s3(bucket_name, content, file_name):
         # Check if the bucket exists
         if not bucket_exists(bucket_name):
             s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'sa-east-1'})
+            set_bucket_lifecycle(bucket_name)
 
         # Determine the content type based on the file extension
         content_type = 'application/zip' if file_name.endswith('.zip') else 'text/csv'
