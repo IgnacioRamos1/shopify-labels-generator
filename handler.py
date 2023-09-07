dca4606060acf4cbcb07a389123e18647d02ec53
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-09-07 11:42:36 -0300

    send email if error is raised

--- handler.py
+++ handler.py
@@ -3,9 +3,15 @@ from utils import get_secret, list_shop_secrets, send_messages_to_sqs
 
 import json
 import logging
+import boto3
+from datetime import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
+
+sns_client = boto3.client('sns')
+
+date = datetime.now().strftime('%Y-%m-%d')
 
 
 def trigger_shop_processing(event, context):
@@ -21,7 +27,16 @@ def trigger_shop_processing(event, context):
             'body': f"Triggered processing for {len(shop_names)} shops."
         }
     except Exception as e:
-        logger.error(f'Error in trigger_shop_processing function: {e}')
+        error_message = f'Error in trigger_shop_processing function: {e}'
+        logger.error(error_message)
+
+        # Publicar mensaje de error en SNS
+        sns_client.publish(
+            TopicArn='arn:aws:sns:sa-east-1:421852645480:LambdaErrorNotifications',
+            Message=error_message,
+            Subject=f'Error in trigger_shop_processing function {date}'
+        )
+
         return {
             'statusCode': 500,
             'body': str(e)
@@ -47,7 +62,15 @@ def process_shop(event, context):
             'body': "CSV files generated and saved to S3"
         }
     except Exception as e:
-        logger.error(f'Error in process_shop function: {e}')
+        error_message = f'Error in store {shop_name} in process_shop function: {e}'
+        logger.error(error_message)
+
+        # Publicar mensaje de error en SNS
+        sns_client.publish(
+            TopicArn='arn:aws:sns:sa-east-1:421852645480:LambdaErrorNotifications',
+            Message=error_message,
+            Subject=f'Error in process_shop function {date}'
+        )
         return {
             'statusCode': 500,
             'body': str(e)
